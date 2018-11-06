863cf77c2fa301b56c7ff206251770f8a77a0b09
A.Schulz <schulz@optimal-systems.de>
2018-11-06 15:32:46 +0100

    prepared the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {EoCoreModule} from '@eo-sdk/core';
+import {FileDropModule} from 'ngx-file-drop';
 import {AppComponent} from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import {AppComponent} from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FileDropModule,
+    EoCoreModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
